46a6cb864540050632d26b035cab1558280230e9
Yamingue kingatal <yamking01@gmail.com>
2021-10-06 00:06:19 +0100

    creation de la compagnie

---
+++ src/Entity/Compagnie.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompagnieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CompagnieRepository::class)
+ */
+class Compagnie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $create_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $update_ate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->create_at;
+    }
+
+    public function setCreateAt(\DateTimeInterface $create_at): self
+    {
+        $this->create_at = $create_at;
+
+        return $this;
+    }
+
+    public function getUpdateAte(): ?\DateTimeInterface
+    {
+        return $this->update_ate;
+    }
+
+    public function setUpdateAte(\DateTimeInterface $update_ate): self
+    {
+        $this->update_ate = $update_ate;
+
+        return $this;
+    }
+}
